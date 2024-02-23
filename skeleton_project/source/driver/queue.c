ff0e4b93e439ca0d3a87bbe9f357a1e5b4d5a66c
jokmoken <157803811+jokmoken@users.noreply.github.com>
2024-02-23 11:23:45 +0100

    Add files via upload

---
+++ skeleton_project/source/driver/queue.c
@@ -0,0 +1,38 @@
+#include <assert.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <pthread.h>
+
+#include "elevio.h"
+#include "con_load.h"
+
+//N_FLOORS = 4
+
+
+//#define MAX_FLOOR 6
+//int floorRequestqueue[] = {11, 21, 22, 31, 32, 42}
+//bool floorRequestqueue[MAX_FLOOR] = {false};
+
+
+typedef enum {
+    NO_ORDER = 0,
+    Call_up = BUTTON_HALL_UP,
+    Call_down = BUTTON_HALL_DOWN,
+    Call_inside_cab = BUTTON_CAB,
+} Orderlist;
+
+
+void addRequest(int floor, Orderlist order){
+
+
+}
+
+void removeRequest(int floor, Orderlist order){
+
+}
+
+
+
---
+++ skeleton_project/source/driver/queue.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "elevio.h"
+#include "con_load.h"
+
+
+
+
+//typedef enum {
+//    No_order = 0;
+//    Call_up = BUTTON_HALL_UP;
+//    Call_down = BUTTON_HALL_DOWN;
+//    Call_inside_cab = BUTTON_CAB;
+//} Orderlist;
